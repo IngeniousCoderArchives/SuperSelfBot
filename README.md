8e3f0f6ce29a3b873b1055e48c8496f40c8f6f97
XtremeCoder <eltontay11@gmail.com>
2019-03-28 18:10:56 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+
+Another Application by IngeniousApplications
+![IA Logo](https://www.ingeniousapps.net/cdn/IALogo.png)
+
+[Discord](https://www.ingeniousapps.net/discord)
+
+[Website](https://www.ingeniousapps.net)
+
+[Donate to us](https://patreon.com/eltontay11)
+# SuperSelfBot
+**Disclaimer : This is not a discord selfbot.**
+
+This bot is a simple bot that makes you online 24/7 (the robot version of your user). It has several utility command such as python eval command, send message / embed command, etc.
+
+## Setup
+1. Fork this repository
+2. Go to https://youtu.be/6za78ipFzg4?t=91. Watch from the 1:31 mark, selecting the forked repository as "deploy from" and setting the config vars as listed below.
+
+### Config Values 
+
+- `prefix` - The bot prefix
+- `owner` - The ID of the owner
+- `game` - Default Game
+- `token` - Bot Token
+
+All configuration values **MUST** be set through Heroku Config Variables.
+## Author
+
+- XtremeCoder
+
+
+## Dependencies
+
+None
+
+## Bug Reporting and Feature Requesting
+Use the Issues tab under this repository to report bugs and suggest features.
+
+## Credits 
+
+Another Quality Application by IngeniousApplications
+
--- bot.py
+++ bot.py
@@ -3,26 +3,90 @@ import os
 from discord.ext import commands
 import io, traceback, textwrap
 from contextlib import redirect_stdout
-bot = commands.Bot(command_prefix="xtreme ",description="IM ONLINE!")
+bot = commands.Bot(command_prefix=os.environ.get("prefix").replace("\"",""),description="An Instance of XtremeCoder's SuperSelfBot, https://github.com/IngeniousCoder/SuperSelfBot")
 @bot.event
 async def on_ready():
-  await bot.change_presence(activity=discord.Game(name=str(f"Maplestory")))
+  await bot.change_presence(activity=discord.Game(name=str(os.environ.get("game"))))
   
   
 @bot.event
 async def on_message(message):
-  if message.author.id == 319794228085653506: await bot.process_commands(message)
+  if message.author.id == int(os.environ.get("owner")): await bot.process_commands(message)
   
+
+@bot.command()
+async def embed(ctx,*,flags):
+    """Sends an embed.
+    Flags :
+    author - set the author name
+    icon - set the icon
+    field:fieldname> - creates a field
+    footer - set the footer
+    footericon - set the footer icon
+    desc - set thee desc
+    title - set the embed title
+    colour - set the embed colour
+    thumbnail - set the thumbnail 
+    image - set the image
+
+    Example :
+    [prefix]embed -title Testing Embed -desc Hi testing
+    """
+    flags = flags.split("-")
+    embed = discord.Embed()
+    for flag in flags:
+        flag_name = flag.split(" ")[0].lower()
+        try:
+          flag_content = flag.split(" ",1)[1]
+        except:
+          continue
+        if flag_name == "author":
+            if embed.author.icon_url == discord.Embed.Empty:
+                embed.set_author(name=flag_content)
+            else:
+                embed.set_author(name=flag_content,icon_url=embed.author.icon_url)
+        if flag_name == "icon":
+            if embed.author.name == discord.Embed.Empty:
+                embed.set_author(name="",icon_url=flag_content)
+            else:
+                embed.set_author(name=embed.author.name,icon_url=flag_content)
+        if flag_name.startswith("field"):
+            # determine name of field
+            name = flag.split(":")[1]
+            name = name.split(">")[0]
+            embed.add_field(name=name,value=flag.split(">")[1])
+        if flag_name == "footer":
+            if embed.footer.icon_url == discord.Embed.Empty:
+                embed.set_footer(text=flag_content)
+            else:
+                embed.set_footer(text=flag_content,icon_url=embed.footer.icon_url)       
+        if flag_name == "footericon":
+            if embed.footer.text == discord.Embed.Empty:
+                embed.set_footer(text="",icon_url=flag_content)
+            else:
+                embed.set_footer(icon_url=flag_content,text=embed.footer.text)
+        if flag_name == "desc":
+            embed.description = flag_content
+        if flag_name == "title":
+            embed.title = flag_content
+        if flag_name == "colour":
+            embed.colour = discord.Color(value=int(flag_content,16))
+        if flag_name == "thumbnail":
+            embed.set_thumbnail(url=flag_content)
+        if flag_name == "image":
+            embed.set_image(url=flag_content)
+    await ctx.send(embed=embed)
+    await ctx.message.delete()
   
 @bot.command()
-async def send(ctx,message):
-    """SEND SOMETHING"""
+async def send(ctx,*,message):
+    """Send something and deletes the command message"""
     await ctx.message.delete()
     await ctx.send(message)
     
     
 @bot.command()
-async def game(ctx,name):
+async def game(ctx,*,name):
     """Set game"""
     await bot.change_presence(activity=discord.Game(name=str(name)))
     
@@ -40,7 +104,7 @@ async def eval(ctx, *, body: str):
         'guild': ctx.message.guild,
         'message': ctx.message,
        }
-    if ctx.message.author.id == 319794228085653506:
+    if ctx.message.author.id == int(os.environ.get("owner")):
       await ctx.message.delete()
       env.update(globals())
 
